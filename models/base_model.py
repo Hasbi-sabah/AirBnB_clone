28a3c043fbf50efcb2233483b738d39d08fe0737
H. Sabah <sabahhasbi00@gmail.com>
2023-08-03 11:27:35 +0000

    added kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,18 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        for key, item in kwargs.items():
+            if key != '__class__':
+                if key == 'created_at' or key == 'updated_at':
+                    d_format = '%Y-%m-%dT%H:%M:%S.%f'
+                    setattr(self, key, datetime.strptime(item, d_format))
+                else:
+                    setattr(self, key, item)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         s = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
@@ -22,7 +30,3 @@ class BaseModel:
         dict_cpy['created_at'] = dict_cpy['created_at'].isoformat()
         dict_cpy['updated_at'] = dict_cpy['updated_at'].isoformat()
         return dict_cpy
-
-if __name__ == "__main__":
-    mod = BaseModel()
-    print(mod.updated_at)
