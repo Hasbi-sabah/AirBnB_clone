2ff7bcaf7c76165c54f1b38bc8e0d1c917c49fdc
GhostProTech <akanniwilliams@gmail.com>
2023-08-06 17:09:29 +0100

    Add the Update feature

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ Console 0.0.1
 """
 import cmd
 import models
+import re
 
 
 class HBNBCommand(cmd.Cmd):
@@ -45,6 +46,7 @@ class HBNBCommand(cmd.Cmd):
         return getattr(models.base_model, cls_name, None)
 
     def do_create(self, cls_name):
+        """ Create a instance"""
         if not cls_name:
             self.missing_name()
         else:
@@ -57,6 +59,7 @@ class HBNBCommand(cmd.Cmd):
                 self.missing_class()
 
     def do_show(self, args):
+        """ Display the requested data"""
         key = self.verify_input(args)
         if key:
             all_objs = models.storage.all()
@@ -66,28 +69,62 @@ class HBNBCommand(cmd.Cmd):
                 self.no_instance()
 
     def do_destroy(self, args):
+        """ Delete an instance"""
         key = self.verify_input(args)
         if key:
             all_objs = models.storage.all()
             try:
                 del all_objs[key]
-                #models.storage.save()
+                models.storage.save()
             except KeyError:
                 self.no_instance()
 
     def do_all(self, args=None):
+        """ Display all objects"""
+        print_model = []
         all_objs = models.storage.all()
         if args:
             cls = self.check_class(args)
             if not cls:
                 return
-            print_model = []
             for key in all_objs.keys():
                 if args == key.split(".")[0]:
                     print_model.append(str(all_objs[key]))
-            print(print_model)
         else:
-            print(all_objs)
+            for key in all_objs.keys():
+                print_model.append(str(all_objs[key]))
+        print(print_model)
+
+    def check_atr(self, args):
+        ins = self.verify_input(args)
+        if ins:
+            atr = args.split()
+            if len(atr) < 3:
+                print("** attribute name missing **")
+            elif len(atr) < 4:
+                print("** value missing **")
+            else:
+                return True
+        return None
+
+    def check_val_typ(self, arg):
+        if re.match(r"^\d+$", arg):
+            return int(args)
+        elif re.match(r"^\d+\.\d+$", arg):
+            return float(arg)
+        else:
+            return arg
+
+    def do_update(self, args):
+        """ Update specific object"""
+        if self.check_atr(args):
+            all_objs = models.storage.all()
+            atr = args.split()
+            obj = "{}.{}".format(atr[0], atr[1])
+            value = self.check_val_typ(atr[3])
+            setattr(all_objs[obj], atr[2], value)
+            models.storage.save()
+
 
     def check_class(self, arg):
         cls = self.get_className(arg)
@@ -104,7 +141,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             self.missing_name()
             return None
-        if len(args) == 2 and len(args) <= 2:
+        if len(args) >= 2:
             cls_id = args[1]
             key = "{}.{}".format(args[0], cls_id)
             return key
