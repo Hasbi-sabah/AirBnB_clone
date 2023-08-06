9dcee23951959a9f8a176d61c92ed2595b4f57c5
GhostProTech <akanniwilliams@gmail.com>
2023-08-06 10:19:27 +0100

    Handle create and show
    known bugs:
    1) show <wrong class>
    output: ** instance id missing ** (wrong)
    expected output: ** class doesn't exist **
    
    2) show <class> <id>
    output contains __class__ (not suppose to be there)

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Console 0.0.1
 """
 import cmd
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -27,6 +28,55 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def missing_class(self):
+        print("** class doesn't exist **")
+
+    def missing_name(self):
+        print("** class name missing **")
+
+    def missing_id(self):
+        print("** instance id missing **")
+
+    def no_instance(self):
+        print("** no instance found **")
+
+    def get_className(self, cls_name):
+            return getattr(models.base_model, cls_name, None)
+
+    def do_create(self, cls_name):
+        if not cls_name:
+            self.missing_name()
+        else:
+            cls = self.get_className(cls_name)
+            if cls:
+                my_model = cls()
+                my_model.save()
+                print(my_model.id)
+            else:
+                self.missing_class()
+
+    def do_show(self, arg):
+        if arg:
+            args = arg.split()
+        else:
+            self.missing_name()
+            return
+        if len(args) == 2 and len(args) <= 2:
+            cls_name = args[0]
+            cls_id = args[1]
+            all_objs = models.storage.all()
+            cls = self.get_className(cls_name)
+            if not cls:
+                self.missing_class()
+            else:
+                key = "{}.{}".format(cls_name, cls_id)
+                try:
+                    print(all_objs[key])
+                except KeyError:
+                    self.no_instance()
+        else:
+            self.missing_id()
 
 if __name__=='__main__':
     HBNBCommand().cmdloop()
