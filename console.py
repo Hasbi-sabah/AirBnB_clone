5085837435419946226bfb6874f0d24f8090943b
H. Sabah <sabahhasbi00@gmail.com>
2023-08-07 18:45:41 +0000

    dict implemented

--- console.py
+++ console.py
@@ -13,32 +13,33 @@ import uuid
 import shlex
 import re
 
+
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
-    cls = ['BaseModel', 'User', 'Place', 'City', 'State', 'Amenity', 'Review']
+    prompt = "(hbnb) "
+    cls = ["BaseModel", "User", "Place", "City", "State", "Amenity", "Review"]
 
     def do_quit(self, line):
-        '''handles quit'''
+        """handles quit"""
         return True
 
     def help_quit(self):
-        print('Handles the quit command and exits')
-    
+        print("Handles the quit command and exits")
+
     def do_EOF(self, line):
-        '''handles EOF'''
+        """handles EOF"""
         print()
         return True
 
     def help_EOF(self):
-        print('Handles the EOF and exits')
+        print("Handles the EOF and exits")
 
     def emptyline(self):
-        '''handles empty lines'''
+        """handles empty lines"""
         pass
 
     def do_create(self, line):
         line_arr = shlex.split(line)
-        if self.is_valid('', line_arr, {}, 1):
+        if self.is_valid("", line_arr, {}, 1):
             new = eval(line_arr[0])()
             new.save()
             print(new.id)
@@ -47,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
         instances = models.storage.all()
         line_arr = shlex.split(line)
         if len(line_arr) >= 2:
-            line = '{}.{}'.format(line_arr[0], line_arr[1])
+            line = "{}.{}".format(line_arr[0], line_arr[1])
         if self.is_valid(line, line_arr, instances, 2):
             print(instances[line])
 
@@ -55,7 +56,7 @@ class HBNBCommand(cmd.Cmd):
         instances = models.storage.all()
         line_arr = shlex.split(line)
         if len(line_arr) >= 2:
-            line = '{}.{}'.format(line_arr[0], line_arr[1])
+            line = "{}.{}".format(line_arr[0], line_arr[1])
         if self.is_valid(line, line_arr, instances, 2):
             del instances[line]
             models.storage.save()
@@ -68,7 +69,10 @@ class HBNBCommand(cmd.Cmd):
             return
         lst = []
         for inst in instances:
-            if len(line_arr) == 0 or (len(line_arr) >= 1 and line_arr[0] == instances[inst].to_dict()['__class__']):
+            if len(line_arr) == 0 or (
+                len(line_arr) >= 1
+                and line_arr[0] == instances[inst].to_dict()["__class__"]
+            ):
                 lst.append(str(instances[inst]))
         print(lst)
 
@@ -76,7 +80,7 @@ class HBNBCommand(cmd.Cmd):
         instances = models.storage.all()
         count = 0
         for inst in instances:
-            if line == instances[inst].to_dict()['__class__']:
+            if line == instances[inst].to_dict()["__class__"]:
                 count += 1
         print(count)
 
@@ -87,53 +91,73 @@ class HBNBCommand(cmd.Cmd):
         else:
             line_arr = line
         if len(line_arr) >= 2:
-            line = '{}.{}'.format(line_arr[0], line_arr[1])
+            line = "{}.{}".format(line_arr[0], line_arr[1])
         if self.is_valid(line, line_arr, instances, 3):
+            line_arr[3] = self.int_float(line_arr[3])
             setattr(instances[line], line_arr[2], line_arr[3])
             models.storage.save()
 
     def default(self, line):
         instances = models.storage.all()
-        line_arr = re.match('^(\w+)\.(\w+)\((.*)\)', line)
+        line_arr = re.match("^(\w+)\.(\w+)\((.*)\)", line)
         if line_arr:
-            line_arr = list(line_arr.groups()) 
-            line_arr[2] = line_arr[2].replace(',', '')
+            line_arr = list(line_arr.groups())
+            line_arr[2] = line_arr[2].replace(",", "")
             line_args = shlex.split(line_arr[2])
-        cmd = {'all': self.do_all, 'count': self.count, 'show': self.do_show, 'destroy': self.do_destroy}
-        if line_arr and len(line_arr) >= 2 and line_arr[0] in self.cls and line_arr[1] in cmd:
-            if len(line_arr) >= 3 and (line_arr[1] == 'show' or line_arr[1] == 'destroy'):
-                arg = line_arr[0] + ' ' + line_arr[2]
+            for i in range(len(line_args)):
+                line_args[i] = line_args[i].strip("{}:")
+        cmd = {
+            "all": self.do_all, "count": self.count,
+            "show": self.do_show, "destroy": self.do_destroy,
+        }
+        if (
+            line_arr
+            and len(line_arr) >= 2
+            and line_arr[0] in self.cls
+            and line_arr[1] in cmd
+        ):
+            if len(line_arr) >= 3 and (
+                line_arr[1] == "show" or line_arr[1] == "destroy"
+            ):
+                arg = line_arr[0] + " " + line_arr[2]
             else:
                 arg = line_arr[0]
             cmd[line_arr[1]](arg)
-        elif line_arr and len(line_arr) >= 2 and line_arr[1] == 'update':
+        elif line_arr and len(line_arr) >= 2 and line_arr[1] == "update":
             if len(line_args) == 1:
                 args = [line_arr[0], line_args[0]]
                 self.do_update(args, 1)
-            for i in range (1, len(line_args), 2):
+            for i in range(1, len(line_args), 2):
                 args = [line_arr[0], line_args[0], line_args[i]]
                 if i + 1 < len(line_args):
                     args.append(line_args[i + 1])
                 self.do_update(args, 1)
         else:
             super().default(line)
-
+    def int_float(self, line):
+        if re.match(r"^\d+$", line):
+            return int(line)
+        elif re.match(r"^\d+\.\d+$", line):
+            return float(line)
+        else:
+            return line
     def is_valid(self, line, line_arr, instances, flag):
         if flag >= 1 and len(line_arr) == 0:
-            print('** class name missing **')
+            print("** class name missing **")
         elif flag >= 1 and line_arr[0] not in self.cls:
             print("** class doesn't exist **")
         elif flag >= 2 and len(line_arr) == 1:
-            print('** instance id missing **')
+            print("** instance id missing **")
         elif flag >= 2 and line not in instances:
-            print('** no instance found **')
+            print("** no instance found **")
         elif flag >= 3 and len(line_arr) == 2:
-            print('** attribute name missing **')
+            print("** attribute name missing **")
         elif flag >= 3 and len(line_arr) == 3:
-            print('** value missing **')
+            print("** value missing **")
         else:
             return True
         return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
