7888ddaf01707092e1e819f9d2f3fbaab9869404
H. Sabah <sabahhasbi00@gmail.com>
2023-08-10 12:42:25 +0000

    console tests

---
+++ tests/test_console.py
@@ -0,0 +1,185 @@
+import uuid
+import os
+import unittest
+from io import StringIO
+from unittest.mock import patch, Mock
+from console import HBNBCommand
+from models.user import User
+import console
+
+class TestConsole(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_storage = Mock()
+        HBNBCommand()._HBNBCommand__session = self.mock_storage
+
+    def tearDown(self):
+        pass
+
+    def test_module_doc(self):
+        self.assertIsNotNone(console.__doc__)
+
+    def test_class_doc(self):
+        self.assertIsNotNone(HBNBCommand.__doc__)
+
+    def test_quit(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("quit")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_EOF(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("EOF")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_empty_line(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_help_create(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help create")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_show(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help show")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_destroy(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help destroy")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_all(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_help_update(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help update")
+            output = f.getvalue().strip()
+            self.assertIsNotNone(output)
+
+    def test_create_with_valid_class_name_BaseModel(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_User(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create User")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_Place(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Place")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_State(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create State")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_City(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create City")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_Amenity(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Amenity")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_with_valid_class_name_Review(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Review")
+            output = f.getvalue().strip()
+            try:
+                uuid.UUID(output)
+            except ValueError:
+                self.fail("Output is not a valid UUID")
+
+    def test_create_without_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_create_with_invalid_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create MyModel")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_show_with_valid_class_and_id(self):
+        test_inst = User()
+        cmd = "show User {self.test_attr.id}"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(cmd)
+            output = f.getvalue().strip()
+            self.assertIn("[User] ({self.test_attr.id})", output)
+
+    def test_show_without_class_name(self):
+        cmd = "show"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(cmd)
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_show_with_invalid_class_name(self):
+        cmd = "show MyModel"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(cmd)
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_show_without_instance_id(self):
+        cmd = "show User"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(cmd)
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_show_with_invalid_instance_id(self):
+        cmd = "show User 121212"
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(cmd)
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+if __name__ == '__main__':
+    unittest.main()
